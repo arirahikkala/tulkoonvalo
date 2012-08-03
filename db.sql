d3fddc96f78d841388571fb923cac0083efbdff0
Ari Rahikkala <ari.rahikkala@gmail.com>
2012-08-03 16:21:04 +0300

    Defined a structure for sliders and slider groups on the server, got it to show on jstree.

--- db.sql
+++ db.sql
@@ -1,7 +1,45 @@
+-- MySQL dump 10.13  Distrib 5.5.25a, for Linux (x86_64)
+--
+-- Host: localhost    Database: webdali
+-- ------------------------------------------------------
+-- Server version	5.5.25a-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!40101 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `sliders`
+--
+
+DROP TABLE IF EXISTS `sliders`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `sliders` (
   `name` text,
-  `brightness` int(11) NOT NULL DEFAULT '0',
-  `groupName` text,
+  `brightness` int(11) DEFAULT NULL,
+  `parent` int(11) DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
+  PRIMARY KEY (`id`),
+  KEY `parent` (`parent`),
+  CONSTRAINT `sliders_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `sliders` (`id`)
+) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2012-08-03 16:20:53
--- server/index.php
+++ server/index.php
@@ -5,7 +5,7 @@ require 'slim/Slim.php';
 $app = new Slim();
 
 $app->get('/programs', 'getPrograms');
-$app->get('/sliders', 'getSliders');
+$app->get('/sliders', 'getSlidersTree');
 $app->run();
 
 function getPrograms() {
@@ -25,15 +25,71 @@ function getPrograms() {
 
 }
 
-function getSliders() {
-	$sql = "select name, brightness, id from sliders";
+// put attributes in a slider into the places where jstree expects them
+function stuffSliderAttributes ($element)
+{
+	$rv = [];
+	foreach ($element as $k => $v) {
+		if ($k == "parent") {}
+		else if ($k == "name") { $rv['data'] = $v; }
+		else if ($k == "children") { $rv['children'] = $v; }
+		else { $rv['attr'][$k] = $v; }
+	}
+
+	return ($rv);
+}
+
+// turn the parent-pointer structure we got from SQL into an actual tree, of a
+// format that can be directly JSON-ed for jstree's use.
+// O(n^2) performance (every item in the list is scanned once for each root, 
+// and each item is a root) - almost certainly ok for the datasets this
+// program will have to deal with.
+// arguments: $root: An array with an 'id' element
+//            $list: An array of arrays with a 'parent' and an 'id' element
+// returns: A tree of elements, with each element's children in ['children']
+function reconstructSliderTree ($root, $list)
+{
+	$root['children'] = [];
+	foreach ($list as $v) {
+		if ($v['parent'] == $root['id']) {
+			$root['children'][] = reconstructSliderTree ($v, $list);
+		}
+	}
+	return stuffSliderAttributes ($root);
+}
+
+function getSlidersTree() {
+	$sql = "select name, brightness, parent, id from sliders";
 	try {
 		$db = getConnection();
 		$stmt = $db->prepare($sql);
 		$stmt->execute();
-		$programs = $stmt->fetchAll (PDO::FETCH_OBJ);
+		$sliders = $stmt->fetchAll (PDO::FETCH_ASSOC);
+		$tree = [];
 
-		echo json_encode ($programs);
+		foreach ($sliders as $key => $s) {
+			if (is_null ($s['parent'])) {
+				$tree[] = reconstructSliderTree ($s, $sliders);
+			}
+		}
+
+		echo json_encode ($tree, JSON_PRETTY_PRINT);
+	}
+	
+	catch(PDOException $e) {
+		echo '{"error":{"text":'. $e->getMessage() .'}}';
+	}
+}
+
+function getSliders() {
+	$sql = "select name, brightness, parent, id from sliders";
+	try {
+		$db = getConnection();
+		$stmt = $db->prepare($sql);
+		$stmt->execute();
+		$sliders = $stmt->fetchAll (PDO::FETCH_OBJ);
+
+		echo json_encode ($sliders, JSON_PRETTY_PRINT);
 	}
 	
 	catch(PDOException $e) {
