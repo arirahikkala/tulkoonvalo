6383166a933ca8b5972f4c28a2e06e5011c0aefe
Tapio Korvala <korvatap@gmail.com>
2012-08-27 20:20:24 +0300

    Added some UI stuff to the structure.

--- server/index.php
+++ server/index.php
@@ -76,6 +76,7 @@ function getPrograms($listwhat = "programs", $program = null, $line = null, $lig
 
 		echo json_encode ($data, JSON_PRETTY_PRINT);
 	}
+
 	
 	catch(Exception $e) {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
