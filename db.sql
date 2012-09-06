3f77ebcabaf7935484f0aba5d123ab37e690abfe
Ari Rahikkala <ari.rahikkala@gmail.com>
2012-09-06 13:00:50 +0300

    New database schema and example data.

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.25a, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.27, for Linux (x86_64)
 --
 -- Host: localhost    Database: webdali
 -- ------------------------------------------------------
--- Server version	5.5.25a-log
+-- Server version	5.5.27-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -27,11 +27,22 @@ CREATE TABLE `lights` (
   `brightness` int(11) DEFAULT NULL,
   `parent` int(11) DEFAULT NULL,
   `id` int(11) NOT NULL AUTO_INCREMENT,
+  `isGroup` tinyint(1) NOT NULL DEFAULT '0',
   PRIMARY KEY (`id`),
   KEY `parent` (`parent`),
   CONSTRAINT `lights_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `lights` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=5 DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `lights`
+--
+
+LOCK TABLES `lights` WRITE;
+/*!40000 ALTER TABLE `lights` DISABLE KEYS */;
+INSERT INTO `lights` VALUES ('All',0,NULL,1,1),('Roomy room',29,1,5,1),('Desk1',85,23,7,0),('Desk2',100,1,8,0),('aaaa',NULL,1,23,1),('ngn',NULL,1,24,1);
+/*!40000 ALTER TABLE `lights` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `programs`
@@ -44,8 +55,18 @@ CREATE TABLE `programs` (
   `name` text,
   `id` int(11) NOT NULL AUTO_INCREMENT,
   PRIMARY KEY (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `programs`
+--
+
+LOCK TABLES `programs` WRITE;
+/*!40000 ALTER TABLE `programs` DISABLE KEYS */;
+INSERT INTO `programs` VALUES ('testiohjelma',1),('toinen testiohjelma',2);
+/*!40000 ALTER TABLE `programs` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `programs_lines`
@@ -62,8 +83,18 @@ CREATE TABLE `programs_lines` (
   PRIMARY KEY (`id`),
   KEY `program_id` (`program_id`),
   CONSTRAINT `programs_lines_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `programs_lines`
+--
+
+LOCK TABLES `programs_lines` WRITE;
+/*!40000 ALTER TABLE `programs_lines` DISABLE KEYS */;
+INSERT INTO `programs_lines` VALUES (1,1,NULL,'5244234234'),(2,2,NULL,'543uoe'),(1,3,NULL,',454a,phs.tnu');
+/*!40000 ALTER TABLE `programs_lines` ENABLE KEYS */;
+UNLOCK TABLES;
 
 --
 -- Table structure for table `programs_lines_lights`
@@ -82,6 +113,16 @@ CREATE TABLE `programs_lines_lights` (
   CONSTRAINT `programs_lines_lights_ibfk_2` FOREIGN KEY (`light_id`) REFERENCES `lights` (`id`)
 ) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `programs_lines_lights`
+--
+
+LOCK TABLES `programs_lines_lights` WRITE;
+/*!40000 ALTER TABLE `programs_lines_lights` DISABLE KEYS */;
+INSERT INTO `programs_lines_lights` VALUES (1,5,10),(3,5,10),(1,7,50),(3,7,60);
+/*!40000 ALTER TABLE `programs_lines_lights` ENABLE KEYS */;
+UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
 /*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
@@ -92,4 +133,4 @@ CREATE TABLE `programs_lines_lights` (
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-08-04 14:28:32
+-- Dump completed on 2012-09-06 13:00:43
