f9c410bf355a39384eba9453b3661b7e3be015d0
Ari Rahikkala <ari.rahikkala@gmail.com>
2013-02-07 13:11:34 +0200

    Starting new database structure (according to current UI and design plans), mock implementation of first new server-side code

--- db.sql
+++ db.sql
@@ -1,8 +1,8 @@
--- MySQL dump 10.13  Distrib 5.5.27, for Linux (x86_64)
+-- MySQL dump 10.13  Distrib 5.5.29, for Linux (x86_64)
 --
 -- Host: localhost    Database: webdali
 -- ------------------------------------------------------
--- Server version	5.5.27-log
+-- Server version	5.5.29-log
 
 /*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
 /*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
@@ -16,6 +16,60 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `groups`
+--
+
+DROP TABLE IF EXISTS `groups`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `groups` (
+  `child_id` char(32) DEFAULT NULL,
+  `parent_id` char(32) DEFAULT NULL,
+  KEY `child_id` (`child_id`),
+  KEY `parent_id` (`parent_id`),
+  CONSTRAINT `groups_ibfk_1` FOREIGN KEY (`child_id`) REFERENCES `lights` (`permanent_id`),
+  CONSTRAINT `groups_ibfk_2` FOREIGN KEY (`parent_id`) REFERENCES `lights` (`permanent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `groups`
+--
+
+LOCK TABLES `groups` WRITE;
+/*!40000 ALTER TABLE `groups` DISABLE KEYS */;
+INSERT INTO `groups` VALUES ('1','3'),('2','3'),('5','4');
+/*!40000 ALTER TABLE `groups` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
+-- Table structure for table `light_activations`
+--
+
+DROP TABLE IF EXISTS `light_activations`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `light_activations` (
+  `current_level` int(11) NOT NULL,
+  `activated_at` timestamp NOT NULL DEFAULT CURRENT_TIMESTAMP ON UPDATE CURRENT_TIMESTAMP,
+  `ends_at` timestamp NOT NULL DEFAULT '0000-00-00 00:00:00',
+  `id` char(32) DEFAULT NULL,
+  KEY `id` (`id`),
+  CONSTRAINT `light_activations_ibfk_1` FOREIGN KEY (`id`) REFERENCES `lights` (`permanent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `light_activations`
+--
+
+LOCK TABLES `light_activations` WRITE;
+/*!40000 ALTER TABLE `light_activations` DISABLE KEYS */;
+INSERT INTO `light_activations` VALUES (50,'2013-02-07 09:54:19','2029-12-31 22:00:00','1'),(100,'2013-02-07 09:54:43','2029-12-31 22:00:00','4');
+/*!40000 ALTER TABLE `light_activations` ENABLE KEYS */;
+UNLOCK TABLES;
+
+--
 -- Table structure for table `lights`
 --
 
@@ -23,15 +77,11 @@ DROP TABLE IF EXISTS `lights`;
 /*!40101 SET @saved_cs_client     = @@character_set_client */;
 /*!40101 SET character_set_client = utf8 */;
 CREATE TABLE `lights` (
-  `name` text,
-  `brightness` int(11) DEFAULT NULL,
-  `parent` int(11) DEFAULT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `isGroup` tinyint(1) NOT NULL DEFAULT '0',
-  PRIMARY KEY (`id`),
-  KEY `parent` (`parent`),
-  CONSTRAINT `lights_ibfk_1` FOREIGN KEY (`parent`) REFERENCES `lights` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=25 DEFAULT CHARSET=utf8;
+  `name` text NOT NULL,
+  `permanent_id` char(32) NOT NULL DEFAULT '',
+  `isGroup` tinyint(1) NOT NULL,
+  PRIMARY KEY (`permanent_id`)
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
 /*!40101 SET character_set_client = @saved_cs_client */;
 
 --
@@ -40,88 +90,8 @@ CREATE TABLE `lights` (
 
 LOCK TABLES `lights` WRITE;
 /*!40000 ALTER TABLE `lights` DISABLE KEYS */;
-INSERT INTO `lights` VALUES ('All',0,NULL,1,1),('Roomy room',29,1,5,1),('Desk1',85,23,7,0),('Desk2',100,1,8,0),('aaaa',NULL,1,23,1),('ngn',NULL,1,24,1);
+INSERT INTO `lights` VALUES ('Aula Etu','1',0),('Aula Taka','2',0),('Aula','3',1),('Eteinen','4',1),('Ulkovalo','5',0);
 /*!40000 ALTER TABLE `lights` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `programs`
---
-
-DROP TABLE IF EXISTS `programs`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `programs` (
-  `name` text,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  PRIMARY KEY (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=3 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `programs`
---
-
-LOCK TABLES `programs` WRITE;
-/*!40000 ALTER TABLE `programs` DISABLE KEYS */;
-INSERT INTO `programs` VALUES ('testiohjelma',1),('toinen testiohjelma',2);
-/*!40000 ALTER TABLE `programs` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `programs_lines`
---
-
-DROP TABLE IF EXISTS `programs_lines`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `programs_lines` (
-  `program_id` int(11) NOT NULL,
-  `id` int(11) NOT NULL AUTO_INCREMENT,
-  `sensor_trigger` text,
-  `time_trigger` text,
-  PRIMARY KEY (`id`),
-  KEY `program_id` (`program_id`),
-  CONSTRAINT `programs_lines_ibfk_1` FOREIGN KEY (`program_id`) REFERENCES `programs` (`id`)
-) ENGINE=InnoDB AUTO_INCREMENT=4 DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `programs_lines`
---
-
-LOCK TABLES `programs_lines` WRITE;
-/*!40000 ALTER TABLE `programs_lines` DISABLE KEYS */;
-INSERT INTO `programs_lines` VALUES (1,1,NULL,'5244234234'),(2,2,NULL,'543uoe'),(1,3,NULL,',454a,phs.tnu');
-/*!40000 ALTER TABLE `programs_lines` ENABLE KEYS */;
-UNLOCK TABLES;
-
---
--- Table structure for table `programs_lines_lights`
---
-
-DROP TABLE IF EXISTS `programs_lines_lights`;
-/*!40101 SET @saved_cs_client     = @@character_set_client */;
-/*!40101 SET character_set_client = utf8 */;
-CREATE TABLE `programs_lines_lights` (
-  `line_id` int(11) NOT NULL,
-  `light_id` int(11) NOT NULL,
-  `brightness` int(11) DEFAULT NULL,
-  KEY `line_id` (`line_id`),
-  KEY `light_id` (`light_id`),
-  CONSTRAINT `programs_lines_lights_ibfk_1` FOREIGN KEY (`line_id`) REFERENCES `programs_lines` (`id`),
-  CONSTRAINT `programs_lines_lights_ibfk_2` FOREIGN KEY (`light_id`) REFERENCES `lights` (`id`)
-) ENGINE=InnoDB DEFAULT CHARSET=utf8;
-/*!40101 SET character_set_client = @saved_cs_client */;
-
---
--- Dumping data for table `programs_lines_lights`
---
-
-LOCK TABLES `programs_lines_lights` WRITE;
-/*!40000 ALTER TABLE `programs_lines_lights` DISABLE KEYS */;
-INSERT INTO `programs_lines_lights` VALUES (1,5,10),(3,5,10),(1,7,50),(3,7,60);
-/*!40000 ALTER TABLE `programs_lines_lights` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
 
@@ -133,4 +103,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2012-09-06 13:00:43
+-- Dump completed on 2013-02-07 13:11:25
--- server2/index.php
+++ server2/index.php
@@ -3,14 +3,81 @@
 require 'slim/Slim.php';
 
 $app = new Slim();
-
+/*
 $app->get('/sliders/', function() { getSliders(); });
 $app->get('/sliders/:id/', function($id) { getSlider($id); });
 $app->put('/sliders/:id/', function($id) { updateSlider($id); });
 $app->post('/sliders/', function() { createNewSlider(); });
+*/
 
+$app->get('/lights/:ids/', function($ids) { getLights($ids); });
 $app->run();
 
+function getLights ($ids) {
+	$foo = array ("lights" => array (
+			      array ("name" => "Aula Etu",
+				     "id" => "1",
+				     "isGroup" => false,
+				     "level" => 50,
+				     "activated_at" => 1360230859,
+				      "ends_at" => 1893448800),
+			      array ("name" => "Aula Taka",
+				     "id" => "2",
+				     "isGroup" => false),
+			      array ("name" => "Aula",
+				     "id" => "3",
+				     "isGroup" => true),
+			      array ("name" => "Eteinen",
+				     "id" => "4",
+				     "isGroup" => true,
+				     "level" => 100,
+				     "activated_at" => 1360230883,
+				     "ends_at" => 1893448800),
+			      array ("name" => "Ulkovalo",
+				     "id" => "5",
+				     "isGroup" => false)),
+		      "groups" => array (
+			      array ("p" => "3",
+				     "c" => "1"),
+			      array ("p" => "3",
+				     "c" => "2"),
+			      array ("p" => "4",
+				     "c" => "5")));
+
+	// TODO: Delimit ids by something safer
+	$ids_array = preg_split ("/,/", $ids);
+
+	$bar = array();
+
+	foreach ($ids_array as $x) {
+		if ($x == "3") {
+			$bar[] = "2";
+			$bar[] = "1";
+		}
+		if ($x == "4") {
+			$bar[] = "5";
+		}
+		$bar[] = $x;
+	}
+	$bar = array_unique ($bar);
+
+	$rv = array ("lights" => array (), "groups" => array ());
+	foreach ($bar as $i) {
+		foreach ($foo["lights"] as $light) {
+			if ($light["id"] == $i) {
+				$rv["lights"][] = $light;
+			}
+		}
+		foreach ($foo["groups"] as $group) {
+			if ($group["p"] == $i)
+				$rv["groups"][] = array ("p" => $i, "c" => $group["c"]);
+		}
+	}
+	print (json_encode ($rv, JSON_PRETTY_PRINT));
+}
+
+
+/*
 function getSliders() {
 	$sql = "select id, value from sliders_test";
 	try {
@@ -80,7 +147,7 @@ function createNewSlider () {
 		echo '{"error":{"text":'. $e->getMessage() .'}}';
 	}
 }
-
+*/
 function getConnection() {
 	$dbh = new PDO("mysql:host=localhost;dbname=webdali", "ari", "foo");	
 	$dbh->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
